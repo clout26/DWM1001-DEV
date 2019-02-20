358a0480047038f1fa39757a0a66fb2a8a5712d0
Mathieu Cloutier <mathieuc@mf-mac-259.connect.momentfactory.local>
2019-02-20 17:08:33 -0500

    Add comment to identify the TAG coordinate

--- DWM1001_host_api/dwm1001_host_api/examples/ex1_TWR_2Hosts/tag/tag_cfg.c
+++ DWM1001_host_api/dwm1001_host_api/examples/ex1_TWR_2Hosts/tag/tag_cfg.c
@@ -9,22 +9,22 @@
  * All rights reserved.
  *
  */
- 
+
 #include "dwm_api.h"
 #include "hal.h"
-      
+
 int main(void)
 {
    int i;
    int wait_period = 1000;
    dwm_cfg_tag_t cfg_tag;
    dwm_cfg_t cfg_node;
-   
+
    HAL_Print("dwm_init(): dev%d\n", HAL_DevNum());
    dwm_init();
-   
+
    HAL_Print("Setting to tag: dev%d.\n", HAL_DevNum());
-   cfg_tag.low_power_en = 0; 
+   cfg_tag.low_power_en = 0;
    cfg_tag.meas_mode = DWM_MEAS_MODE_TWR;
    cfg_tag.loc_engine_en = 1;
    cfg_tag.common.led_en = 1;
@@ -33,54 +33,55 @@ int main(void)
    cfg_tag.common.fw_update_en = 0;
    HAL_Print("dwm_cfg_tag_set(&cfg_tag): dev%d.\n", HAL_DevNum());
    dwm_cfg_tag_set(&cfg_tag);
-   
+
    HAL_Print("Wait 2s for node to reset.\n");
-   HAL_Delay(2000);  
+   HAL_Delay(2000);
    dwm_cfg_get(&cfg_node);
-   
+
    HAL_Print("Comparing set vs. get: dev%d.\n", HAL_DevNum());
-   if((cfg_tag.low_power_en        != cfg_node.low_power_en) 
-   || (cfg_tag.meas_mode           != cfg_node.meas_mode) 
-   || (cfg_tag.loc_engine_en       != cfg_node.loc_engine_en) 
-   || (cfg_tag.common.led_en       != cfg_node.common.led_en) 
-   || (cfg_tag.common.ble_en       != cfg_node.common.ble_en) 
-   || (cfg_tag.common.uwb_mode     != cfg_node.common.uwb_mode) 
+   if((cfg_tag.low_power_en        != cfg_node.low_power_en)
+   || (cfg_tag.meas_mode           != cfg_node.meas_mode)
+   || (cfg_tag.loc_engine_en       != cfg_node.loc_engine_en)
+   || (cfg_tag.common.led_en       != cfg_node.common.led_en)
+   || (cfg_tag.common.ble_en       != cfg_node.common.ble_en)
+   || (cfg_tag.common.uwb_mode     != cfg_node.common.uwb_mode)
    || (cfg_tag.common.fw_update_en != cfg_node.common.fw_update_en))
    {
-      HAL_Print("low_power_en        cfg_tag=%d : cfg_node=%d\n", cfg_tag.low_power_en,     cfg_node.low_power_en); 
-      HAL_Print("meas_mode           cfg_tag=%d : cfg_node=%d\n", cfg_tag.meas_mode,        cfg_node.meas_mode); 
-      HAL_Print("loc_engine_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.loc_engine_en,    cfg_node.loc_engine_en); 
-      HAL_Print("common.led_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.led_en,    cfg_node.common.led_en); 
-      HAL_Print("common.ble_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.ble_en,    cfg_node.common.ble_en); 
-      HAL_Print("common.uwb_mode     cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.uwb_mode,  cfg_node.common.uwb_mode); 
-      HAL_Print("common.fw_update_en cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.fw_update_en, cfg_node.common.fw_update_en);  
+      HAL_Print("low_power_en        cfg_tag=%d : cfg_node=%d\n", cfg_tag.low_power_en,     cfg_node.low_power_en);
+      HAL_Print("meas_mode           cfg_tag=%d : cfg_node=%d\n", cfg_tag.meas_mode,        cfg_node.meas_mode);
+      HAL_Print("loc_engine_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.loc_engine_en,    cfg_node.loc_engine_en);
+      HAL_Print("common.led_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.led_en,    cfg_node.common.led_en);
+      HAL_Print("common.ble_en       cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.ble_en,    cfg_node.common.ble_en);
+      HAL_Print("common.uwb_mode     cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.uwb_mode,  cfg_node.common.uwb_mode);
+      HAL_Print("common.fw_update_en cfg_tag=%d : cfg_node=%d\n", cfg_tag.common.fw_update_en, cfg_node.common.fw_update_en);
       HAL_Print("\nConfiguration failed.\n\n");
    }
    else
    {
       HAL_Print("\nConfiguration succeeded.\n\n");
    }
-   
+
    dwm_loc_data_t loc;
    dwm_pos_t pos;
    loc.p_pos = &pos;
    while(1)
-   {      
+   {
       HAL_Print("Wait %d ms...\n", wait_period);
-      HAL_Delay(wait_period);        
-   
+      HAL_Delay(wait_period);
+
       HAL_Print("dwm_loc_get(&loc):\n");
 
       if(dwm_loc_get(&loc) == RV_OK)
       {
          HAL_Print("\t[%d,%d,%d,%u]\n", loc.p_pos->x, loc.p_pos->y, loc.p_pos->z,
                loc.p_pos->qf);
+        HAL_Print("WAS THE TAG UP THER\n\n\n");
 
-         for (i = 0; i < loc.anchors.dist.cnt; ++i) 
+         for (i = 0; i < loc.anchors.dist.cnt; ++i)
          {
             HAL_Print("\t%u)", i);
             HAL_Print("0x%llx", loc.anchors.dist.addr[i]);
-            if (i < loc.anchors.an_pos.cnt) 
+            if (i < loc.anchors.an_pos.cnt)
             {
                HAL_Print("[%d,%d,%d,%u]", loc.anchors.an_pos.pos[i].x,
                      loc.anchors.an_pos.pos[i].y,
@@ -91,6 +92,6 @@ int main(void)
          }
       }
    }
-   
-   return 0;   
+
+   return 0;
 }
